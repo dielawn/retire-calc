3eb78e81729a7f367bb0ca40ea5f17f7f86368fc
dielawn <dielawnmerkill@gmail.com>
2023-04-02 16:28:05 -0600

    Data to table, no formating, no inputs hooked up

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-const compoundSavings = (initialSavings, monthlyContribution, rateOfGrowth, years) => {   
+
+const savingsData = [];
+
+  const compoundSavings = (initialSavings, monthlyContribution, rateOfGrowth, years) => {   
     let totalSavings = initialSavings;
     let annualContribution = monthlyContribution * 12;
+    
     for(let i = 0; i < years; i++){
-        let growth = totalSavings * rateOfGrowth;
+        let growth = (totalSavings + annualContribution) * rateOfGrowth;
         totalSavings = totalSavings + annualContribution + growth;
+        const annualData = {
+            year: i + 1,
+            contribution: annualContribution,
+            growth: growth.toFixed(2),
+            savings: totalSavings.toFixed(2)
+
+        }
+        savingsData.push(annualData)
+        
         console.log(`Year ${i + 1}: Growth = ${growth.toFixed(2)}, Total Savings = ${totalSavings.toFixed(2)}`);
+        
     }
+    console.table(savingsData)
+   return savingsData
 };
-compoundSavings(0, 600, 0.02, 15);
+
+
+const renderTable = () => {
+  const tableBody = document.getElementById('savingsTable');
+  // clear existing rows
+  tableBody.innerHTML = '';
+
+  savingsData.forEach((data) => {
+    console.log('hi')
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${data.year}</td>
+      <td>${data.contribution}</td>
+      <td>${data.growth}</td>
+      <td>${data.savings}</td>`;
+    tableBody.appendChild(newRow);
+  });
+};
+
+
+// compoundSavings(19282, 600, 0.02, 30);
+
+document.getElementById('calculate').addEventListener('click', () =>{
+compoundSavings(19282, 600, 0.02, 30);
+ renderTable()
+})
+
+document.getElementById('clear').addEventListener('click', () =>{
+    
+})
+
+
+  
+
+  
